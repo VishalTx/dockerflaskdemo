12cf675e0a81f1a9d90c8cd032aa447f130c2519
gsingh <ipreetgs.gmail.com>
2022-09-07 11:24:56 +0530

    ds

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-from flask import Flask
-import os
-import socket
+from flask import *
 
 app = Flask(__name__)
+app.config["Debug"] = True
 
-@app.route("/")
-def hello():
-    html = "<h2>HTML Images</h2>"
-            "<p>HTML images are defined with the img tag:</p>"
-    return html
 
-PORT = int(os.environ.get("PORT", 9090))
+@app.route('/')
+def mydef():
+    return "hello page"
 if __name__ == '__main__':
-    app.run(threaded=True,host='0.0.0.0',port=PORT)
-
-# if __name__ == "__main__":
-#   app.run(host='0.0.0.0', port=8080)
+    app.run()

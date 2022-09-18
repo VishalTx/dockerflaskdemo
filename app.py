23d4d9792d02f2368c9beacbba75122b1068d621
vishal.sader <vishal.sader@testingxperts.com>
2022-09-18 18:26:32 +0530

    added templates

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 # app.config["Debug"] = True
 
 @app.route('/')
-def mydef():
-    return "hello page this is python app"
+def hello():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run()
 
